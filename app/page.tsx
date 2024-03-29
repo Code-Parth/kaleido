af88bd33915f68cd4b0ff7fbd9d5a8f310abb020
imsp18 <shivangparmar537@gmail.com>
2024-03-30 02:40:29 +0530

    create connect wallet using rainbowkit and after auth goes to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,30 @@
+"use client";
+
 import Image from 'next/image';
-// import { useAccount } from "wagmi";
-// import { useAccount } from 'wagmi';
+import { useEffect } from 'react';
+import { useAccount } from "wagmi";
+import { useRouter } from 'next/navigation';
+import { Space_Grotesk } from 'next/font/google';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
-import { Inter, Space_Grotesk } from 'next/font/google';
-// import { useAccount, useDisconnect, useEnsAvatar, useEnsName } from 'wagmi'
 
 const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
 
 export default function Home() {
-    // const { address } = useAccount()
+    const { address, isConnected } = useAccount();
+
+    if (isConnected) {
+        console.log(`Connected with address: ${address}`);
+    }
+
+    const router = useRouter();
+    useEffect(() => {
+        if (isConnected) {
+            router.push('/home');
+        }
+    }, [isConnected, router]);
 
     return (
-        <div className='flex flex-col justify-center'>
+        <div className='flex flex-col  justify-center'>
             {/* Navbar */}
             <div className={`${spaceGrotesk.className} flex justify-center items-center w-full text-black text-2xl font-bold mt-4 gap-1`}>
                 <Image src="/assets/logo.svg" alt='logo' width={32} height={32} />
@@ -20,14 +33,14 @@ export default function Home() {
             {/* Hero */}
             <div className='flex mt-24 items-center justify-between max-sm:flex-col max-sm:mt-12'>
                 {/* Text Part */}
-                <div className='flex flex-col gap-4 w-1/2 max-sm:w-full'>
+                <div className='flex flex-col gap-4 w-1/2 items-center max-sm:w-full'>
                     {/* Image 1 */}
                     {/* Header */}
-                    <h1 className='text-6xl max-sm:text-3xl font-bold text-[#6F00FF]'>{`Reimagine what's possible`}</h1>
+                    <h1 className={`${spaceGrotesk.className} text-6xl max-sm:text-center max-sm:text-3xl font-bold text-[#6F00FF]`}>{`Reimagine what's possible`}</h1>
                     {/* Paragraph */}
-                    <p className='text-2xl max-sm:text-base'>Kaleido empowers you to generate unique content, connect with a vibrant community, and own your creations as NFTs.</p>
+                    <p className='text-2xl max-sm:text-base max-sm:text-center'>Kaleido empowers you to generate unique content, connect with a vibrant community, and own your creations as NFTs.</p>
                     {/* Button */}
-                    <ConnectButton  />
+                    <ConnectButton />
                 </div>
                 {/* Image Part */}
                 <Image src='/assets/hero.png' alt='hero' width={537} height={522} />
