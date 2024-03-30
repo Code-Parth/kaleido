a8d7a1533d624856f16ae08cd16d9e030858a775
Code-Parth <softsparths@gmail.com>
2024-03-31 05:25:03 +0530

    Refactor NFT fetching and add file-saver dependency

--- app/(dashboard)/home/page.jsx
+++ app/(dashboard)/home/page.jsx
@@ -3,38 +3,10 @@
 import axios from "axios";
 import { useState } from "react";
 import MarketplaceJSON from "@/Marketplace.json";
+// import { json } from "stream/consumers";
 
 export default function Marketplace() {
-    const sampleData = [
-        {
-            "name": "NFT#1",
-            "description": "Cute Dog",
-            "website": "http://axieinfinity.io",
-            "image": "https://gateway.pinata.cloud/ipfs/QmTcQR1wMsnxakNikzePPJee77NxDtuRBkzvWeHva9vmfq",
-            "price": "0.03ETH",
-            "currentlySelling": "True",
-            "address": "0xe81Bf5A757CB4f7F82a2F23b1e59bE45c33c5b13",
-        },
-        {
-            "name": "NFT#2",
-            "description": "Fantastic Dog",
-            "website": "http://axieinfinity.io",
-            "image": "https://gateway.pinata.cloud/ipfs/QmdUXeAUvjiwmS3htSYECxrr6ewjF27B3FYhuLfF33W1Bq",
-            "price": "0.03ETH",
-            "currentlySelling": "True",
-            "address": "0xe81Bf5A757C4f7F82a2F23b1e59bE45c33c5b13",
-        },
-        {
-            "name": "NFT#3",
-            "description": "Next Generation of Dog",
-            "website": "http://axieinfinity.io",
-            "image": "https://gateway.pinata.cloud/ipfs/QmbkEvueGNBbHduz3DAUX4vVzcYn8M4CVfitzQP1nw9tRG",
-            "price": "0.03ETH",
-            "currentlySelling": "True",
-            "address": "0xe81Bf5A757C4f7F82a2F23b1e59bE45c33c5b13",
-        },
-    ];
-    const [data, updateData] = useState(sampleData);
+    const [data, updateData] = useState([]);
     const [dataFetched, updateFetched] = useState(false);
 
     async function getAllNFTs() {
@@ -50,29 +22,45 @@ export default function Marketplace() {
         // Fetch all the details of every NFT from the contract and display
         const items = await Promise.all(transaction.map(async i => {
             const tokenURI = await contract.tokenURI(i.tokenId);
-            let meta = await axios.get(tokenURI);
-            meta = meta.data;
+            // console.log(tokenURI);
+            const meta = fetch(tokenURI).then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+                .then(data => {
+                    console.log('Data received:', data);
+                })
+                .catch(error => {
+                    console.error('There was a problem with the fetch operation:', error);
+                });
+            // console.log("----------------------------",meta);
+            // const meta = await axios.get(tokenURI);
+            // console.log(meta);
 
-            let price = ethers.utils.formatUnits(i.price.toString(), 'ether');
-            let item = {
-                price,
-                tokenId: i.tokenId.toNumber(),
-                seller: i.seller,
-                owner: i.owner,
-                image: meta.image,
-                name: meta.name,
-                description: meta.description,
-            }
-            return item;
+            // let price = ethers.utils.formatUnits(i.price.toString(), 'ether');
+            // let item = {
+            //     price,
+            //     tokenId: i.tokenId.toNumber(),
+            //     seller: i.seller,
+            //     owner: i.owner,
+            //     image: meta.image,
+            //     name: meta.name,
+            //     description: meta.description,
+            // }
+            // return item;
         }))
 
-        updateFetched(true);
-        updateData(items);
+        // updateFetched(true);
+        // updateData(items);
     }
 
     if (!dataFetched) {
         getAllNFTs();
     }
+
+    // console.log(data);
 
     return (
         <>
