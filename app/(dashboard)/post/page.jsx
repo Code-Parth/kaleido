45643a59cc1b446c1763c1f54d609e52ad2c0c9b
Code-Parth <softsparths@gmail.com>
2024-03-31 07:45:29 +0530

    Add Sidebar component to tsconfig.json and update form styling in post/page.jsx

--- app/(dashboard)/post/page.jsx
+++ app/(dashboard)/post/page.jsx
@@ -96,15 +96,15 @@ export default function SellNFT() {
                 <Sidebar />
                 <div className="flex w-full h-screen justify-center items-center">
                     <div className="flex flex-col place-items-center mt-10" id="nftForm">
-                        <form className="bg-white shadow-md rounded px-8 pt-4 pb-8 mb-4">
-                            <h3 className="text-center font-bold text-purple-500 mb-8">Upload your NFT to the marketplace</h3>
+                        <form className="shadow-md rounded px-8 pt-4 pb-8 mb-4">
+                            {/* <h3 className="text-center font-bold text-purple-500 mb-8">Upload your NFT to the marketplace</h3> */}
                             <div className="mb-4">
-                                <label className="block text-purple-500 text-sm font-bold mb-2" htmlFor="name">NFT Name</label>
-                                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="KLD#4563" onChange={e => updateFormParams({ ...formParams, name: e.target.value })} value={formParams.name}></input>
+                                <label className="block text-sm font-semibold leading-6 text-gray-900" htmlFor="name">NFT Name</label>
+                                <input className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" id="name" type="text" placeholder="KLD#4563" onChange={e => updateFormParams({ ...formParams, name: e.target.value })} value={formParams.name}></input>
                             </div>
                             <div className="mb-6">
-                                <label className="block text-purple-500 text-sm font-bold mb-2" htmlFor="description">NFT Description</label>
-                                <textarea className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" cols="40" rows="5" id="description" type="text" placeholder="NFT Description" value={formParams.description} onChange={e => updateFormParams({ ...formParams, description: e.target.value })}></textarea>
+                                <label className="block text-sm font-semibold leading-6 text-gray-900" htmlFor="description">Post Description</label>
+                                <textarea className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" cols="40" rows="5" id="description" type="text" placeholder="NFT Description" value={formParams.description} onChange={e => updateFormParams({ ...formParams, description: e.target.value })}></textarea>
                             </div>
                             {/* <div className="mb-6">
                                 <label className="block text-purple-500 text-sm font-bold mb-2" htmlFor="price">Price (in ETH)</label>
